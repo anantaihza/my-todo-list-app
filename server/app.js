457d65a6da454bfb959c0d9e435d242a7dc344ac
Ananta Ihza <anantaihza21@gmail.com>
2024-09-25 20:26:37 +0700

    fix: delete file vercel.json

--- server/app.js
+++ server/app.js
@@ -2,8 +2,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const port = process.env.PORT || 3000;
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -14,9 +12,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/', router);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 module.exports = app;
