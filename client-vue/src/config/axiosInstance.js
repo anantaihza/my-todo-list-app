0050fa311c201e58dfbd8c541b7a5bbab9e58a3f
Ananta Ihza <anantaihza21@gmail.com>
2024-09-25 21:39:57 +0700

    feat: change baseURL

--- client-vue/src/config/axiosInstance.js
+++ client-vue/src/config/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  // baseURL: 'http://localhost:3000'
+  baseURL: 'http://34.101.36.170'
 })
 
 export default instance
