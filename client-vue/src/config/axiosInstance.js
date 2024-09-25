2252ccdcb965a23fc212cc15bf19c9793ddf16c9
Ananta Ihza <anantaihza21@gmail.com>
2024-09-25 22:45:26 +0700

    fix: baseURL axios

--- client-vue/src/config/axiosInstance.js
+++ client-vue/src/config/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   // baseURL: 'http://localhost:3000'
-  baseURL: 'http://34.101.36.170'
+  baseURL: 'https://todo-list-server.anantair.my.id'
 })
 
 export default instance
